6e20a356d1087e826555634201d08436f782a80a
Sam Chillcott <samchillcott@gmail.com>
2021-07-16 09:28:42 +0100

    add css and nav components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import People from './components/People';
+import Planets from './components/Planets';
 
 function App() {
+  const [page, setPage] = useState('planets')
   return (
     <div className="App">
       <h1>Star Wars Info</h1>
-      <Navbar />
+      <Navbar setPage={setPage}/>
       <div className="content">
-
+        { page === 'planets' ? <Planets /> : <People />}
       </div>
     </div>
   );
